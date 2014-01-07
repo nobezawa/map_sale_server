ff6945d119aff17dcb63a9842cf67c6bd61a561e
nobezawa <charmy.nobezawa@gmail.com>
2014-01-07 18:23:00 +0900

    company controller test

--- spec/controllers/companies_controller_spect.rb
+++ spec/controllers/companies_controller_spect.rb
@@ -16,18 +16,42 @@ describe CompaniesController do
   end
 
   describe "GET 'new'", type: :new do
-  	it "returns http success" do
-  	  get 'index'
-  	  response.should be_success
-  	end
-
-    it "new success" do
-      get :new, {}, valid_session
-      assigns[:company].should be_a_new(Company)
+    it "returns http success" do
+      get 'new'
+      response.should be_success
     end
 
+    it "new success" do
+      get 'new'
+      expect(assigns[:company]).to be_a_new(Company)
+    end
   end
 
+  describe "POST 'create'", type: :create do
+    before do
+      @params = {
+        :name => 'aaaaa',
+        :prefecture => 1,
+        :address1 => 'aaa',
+        :address2 => 'aaa',
+        :tel => '09012341234',
+        :email => 'sample@gmail.com'
+      }
+    end
+    
+    it "create a new Company" do
+      expect {post :create, {:company => @params}}.to change(Company, :count).by(1)
+    end
+
+    context 'save company' do
+      it "redirect show last" do
+        post :create, :company => @params
+        expect(response).to be_redirect
+        expect(response).to redirect_to(assigns[:company])
+      end
+    end
+    
+  end
 
 
 end
